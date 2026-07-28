2dccf44da8f954d92c344809c2455015ff00b91c
Farisa-11 <farisaalmas@gmail.com>
2026-07-28 15:35:22 +0530

    Time: 2 ms (99.95%) | Memory: 52.7 MB (40.94%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,16 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {//key-element, value-frequency
-        HashMap<Integer,Integer> map=new HashMap<>();
+        int c1=0;
+        int c2=0;
+        int count1=0;
+        int count2=0;
+        for(int num: nums){
+            if(c1==num){
+                count1++;
+            }
+            else if(c2==num){
+                count2++;
+            }
+            else if(count1==0){
+                c1=num;
+                count1=1;
+            }
+            else if(count2==0){
+                c2=num;
+                count2=1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
         for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+            if(num==c1){
+                count1++;
+            }
+            else if (num==c2){
+                count2++;
+            }
         }
             List<Integer> ans=new ArrayList<>();
-            int limit=nums.length/3;
-            for(int key: map.keySet()){
-                if(map.get(key)>limit){
-                    ans.add(key);
-                }
+            if(count1>nums.length/3){
+                ans.add(c1);
             }
+            if(count2>nums.length/3){
+                ans.add(c2);
+            }
+        
             return ans;
         }
     }
